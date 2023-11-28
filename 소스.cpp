b92d36a487a80da00c6417499efecb1997bf3c61
KyungMin Yun <110750328+penguinman12@users.noreply.github.com>
2023-11-29 00:28:41 +0900

    Update 소스.cpp

--- 소스.cpp
+++ 소스.cpp
@@ -25,6 +25,7 @@ Platform ground(0.0f, -boundaryY + PLAYER_SIZE * 0.5f, 0.0f, boundaryX * 2, PLAY
 Platform ground2(-boundaryY + PLAYER_SIZE * 0.5f, 0.0f, 0.0f, PLAYER_SIZE, boundaryY * 2);
 Platform ground3(0.0f, boundaryY - PLAYER_SIZE * 0.5f, 0.0f, boundaryX * 2, PLAYER_SIZE);
 Platform ground4(boundaryY - PLAYER_SIZE * 0.5f, 0.0f, 0.0f, PLAYER_SIZE, boundaryY * 2);
+Platform F1(-260, -140, 0, 40, 40);
 Player player1(-boundaryX / 3, -boundaryY + PLAYER_SIZE * 5.0f, 0.0f, PLAYER_SIZE);
 Player player2(boundaryX / 3, -boundaryY + PLAYER_SIZE * 5.0f, 0.0f, PLAYER_SIZE);
 
@@ -39,25 +40,18 @@ Texture texture2;
 Texture playerf1;
 Texture playerf2;
 
-void togglePl(int value) {
-	pl = (pl + 1) % 2;
-	if (pl)
-		player1.shootBubble();
-	else
-		player2.shootBubble();
 
-	glutPostRedisplay();
-	glutTimerFunc(1000, togglePl, 1);
-}
 
 bool isCollisionDetected(const Player& player, const Platform& platform) {
 	/* Implement: collision detection between player and platform */
-	if ((player.getCenter()[1] - PLAYER_SIZE / 2) <= (platform.getCenter()[1] + platform.getHeight() / 2))
+	if (((player.getCenter()[1] - PLAYER_SIZE / 2) <= (platform.getCenter()[1] + platform.getHeight() / 2)) && (abs(player.getCenter()[0] - platform.getCenter()[0]) <= player.getSize() / 2.0f + platform.getWidth() / 2.0f))
 		return true;
 	else
+		
 		return false;
 
 }
+
 
 void handleCollision(Player& player, const Platform& platform) {
 	/* Implement: collision handling of player */
@@ -66,8 +60,13 @@ void handleCollision(Player& player, const Platform& platform) {
 		player.setVelocity(Vector3f(0.0f, 0.0f, 0.0f));
 		player.setAcceleration(Vector3f(0.0f, 0.0f, 0.0f));
 		player.setVerticalState(Player::STOPJ);
-		cout << '1';
 	}
+	else if (!isCollisionDetected(player, platform) && player.isStop()) {
+		player.setVerticalState(Player::FALL);
+		player.setAcceleration(Vector3f(0.0f, -0.5f, 0.0f));
+	}
+	
+	
 }
 
 
@@ -85,22 +84,28 @@ void idle() {
 	end_t = clock();
 
 	if ((float)(end_t - start_t) > 1000 / 30.0f) {
-		
+		if (player.getVelocity()[1] == 0.0f && player.isJumping())
+			cout << player.getCenter()[1];
 		player.move();
 		if (player.getVelocity()[1] < 0) {
 			player.setVerticalState(Player::VERTICAL_STATE::FALL);
 		}
+		
+		
 		handleCollision(player, ground);
+		handleCollision(player, F1);
 
 		for (int i = 0; i < bubbles.size(); i++) {
 			bubbles[i].move();
 			//버블 크기 조절
-			if (bubbles[i].getRadius() <= PLAYER_SIZE / 2)
+			if (bubbles[i].getRadius() < PLAYER_SIZE / 2)
 				bubbles[i].setRadius(bubbles[i].getRadius() + 0.5f);
-			//버블 y축 이동
-			if (bubbles[i].getCenter()[0] + bubbles[i].getRadius() >= boundaryX )
+			if (bubbles[i].getRadius() == PLAYER_SIZE / 2)
 				bubbles[i].setVelocity(Vector3f(0.0f, 3.0f, 0.0f));
-			if (bubbles[i].getCenter()[0] - bubbles[i].getRadius() <= -boundaryX )
+			//버블 y축 이동
+			if (bubbles[i].getCenter()[0] + bubbles[i].getRadius() >= boundaryX - PLAYER_SIZE)
+				bubbles[i].setVelocity(Vector3f(0.0f, 3.0f, 0.0f));
+			if (bubbles[i].getCenter()[0] - bubbles[i].getRadius() <= -boundaryX +PLAYER_SIZE)
 				bubbles[i].setVelocity(Vector3f(0.0f, 3.0f, 0.0f));
 		}
 
@@ -145,6 +150,7 @@ void display() {
 		ground2.draw();
 		ground3.draw();
 		ground4.draw();
+		F1.draw();
 		player.draw();
 
 	}
@@ -272,8 +278,8 @@ void specialKeyDown(int key, int x, int y) {
 	case GLUT_KEY_UP:
 		if (player.isStop()) {
 			player.setVerticalState(Player::VERTICAL_STATE::JUMP);
-			player.setVelocity(Vector3f(0, 20, 0));
-			player.setAcceleration(Vector3f(0, -1, 0));
+			player.setVelocity(Vector3f(0, 13, 0));
+			player.setAcceleration(Vector3f(0, -0.5f, 0));
 			
 		}
 
