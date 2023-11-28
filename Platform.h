f6c3aa7d168283c0b084f7629f2e58ac525b9627
KyungMin Yun <110750328+penguinman12@users.noreply.github.com>
2023-11-28 15:10:58 +0900

    Add files via upload

---
+++ Platform.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Vector3f.h"
+#include "Shape3D.h"
+
+class Platform:public Shape3D
+{
+public:
+	Platform(float x, float y, float z, float w, float h);
+
+	float getWidth() const;
+	float getHeight() const;
+
+
+	void draw() const;
+
+private:
+	Vector3f center;
+
+	float width, height;
+};
+
