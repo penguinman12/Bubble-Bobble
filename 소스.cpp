8ad49bd3d1b84805a948913edd38ca19e8d65ad4
KyungMin Yun <110750328+penguinman12@users.noreply.github.com>
2023-11-30 20:07:38 +0900

    Update 소스.cpp

--- 소스.cpp
+++ 소스.cpp
@@ -99,6 +99,19 @@ void enemyMove(Enemy& enemy, Platform& platform) {
 	}
 }
 
+void minuslife(Enemy& enemy, Player& player) {
+	if (abs(player.getCenter()[0] - enemy.getCenter()[0]) < player.getSize() / 2.0f + enemy.getSize() / 2.0f &&
+		abs(player.getCenter()[1] - enemy.getCenter()[1]) < player.getSize() / 2.0f + enemy.getSize() / 2.0f) {
+		life--;
+
+		player.setCenter(Vector3f(-boundaryX + PLAYER_SIZE * 1.5f, -boundaryY + PLAYER_SIZE * 1.5f, 0.0f));
+		enemy.setCenter(Vector3f(0, 0, 0)); 
+
+		
+	}
+}
+
+
 void initialize() {
 	player.setVerticalState(Player::VERTICAL_STATE::STOPJ);
 	player.setAcceleration(Vector3f(0.0f, 0.0f, 0.0f));
@@ -121,7 +134,6 @@ void idle() {
 
 	if ((float)(end_t - start_t) > 1000 / 30.0f) {
 		if (player.getVelocity()[1] == 0.0f && player.isJumping())
-			cout << player.getCenter()[1];
 		player.move();
 		if (player.getVelocity()[1] < 0) {
 			player.setAcceleration(Vector3f(0.0f, -0.2f, 0.0f));
@@ -427,7 +439,6 @@ void specialKeyUp(int key, int x, int y) {
 	case GLUT_KEY_RIGHT:
 		player.setHorizontalState(Player::HORIZONTAL_STATE::STOP);
 		player.setVelocity(Vector3f(0.0f, player.getVelocity()[1], 0));
-		cout << player.isFalling();
 		break;
 	case GLUT_KEY_LEFT:
 		player.setHorizontalState(Player::HORIZONTAL_STATE::STOP);
